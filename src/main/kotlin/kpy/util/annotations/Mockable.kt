d7770bf6dcf25c8620baafcb1934a508283f5de3
ghasem <shirdelghasem79@gmail.com>
2021-07-21 21:13:10 +0430

    fix bug

--- src/main/kotlin/kpy/util/annotations/Mockable.kt
+++ src/main/kotlin/kpy/util/annotations/Mockable.kt
@@ -1,4 +1,17 @@
 package kpy.util.annotations
 
-/** Mockable annotate make classes open for inheritance or mock testing */
+/**
+ * ### Mockable annotate make classes open for inheritance or mock testing
+ * * configuration in build.gradle
+ * ```
+ * plugins {
+ *  ...
+ *  id 'org.jetbrains.kotlin.plugin.allopen' version "VERSION"
+ *  ...
+ * }
+ * allOpen {
+ *  annotation("kpy.util.annotations.Mockable")
+ * }
+ * ```
+ */
 annotation class Mockable
