287a785df7e12f1cf29eb74275062a78bd73182e
ghasem <shirdelghasem79@gmail.com>
2021-07-21 20:58:07 +0430

    add mockable annotate

---
+++ src/main/kotlin/kpy/util/annotations/Mockable.kt
@@ -0,0 +1,4 @@
+package kpy.util.annotations
+
+/** Mockable annotate make classes open for inheritance or mock testing */
+annotation class Mockable
---
+++ src/test/kotlin/kpy/annotations/MockableKtTest.kt
@@ -0,0 +1,18 @@
+package kpy.annotations
+
+import com.google.common.truth.Truth.assertThat
+import kpy.util.annotations.Mockable
+import org.junit.jupiter.api.Test
+
+class MockableTest {
+
+    @Test
+    fun `TA extended Student`() {
+        assertThat(TA()).isInstanceOf(Student::class.java)
+    }
+}
+
+@Mockable
+class Student
+
+class TA : Student()
