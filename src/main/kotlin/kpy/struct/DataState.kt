a6bc0f54a58f54687bc9d112ad75a6b1ab23ee95
ghasem <shirdelghasem79@gmail.com>
2021-07-22 11:31:31 +0430

    Update DataState.kt

--- src/main/kotlin/kpy/struct/DataState.kt
+++ src/main/kotlin/kpy/struct/DataState.kt
@@ -2,17 +2,17 @@ package kpy.struct
 
 /**
  * DataState has three mode
- * + Success : return data and status
+ * + Success : return data
  * + Error   : throw exception
  * + Loading : loading and not coming
  * @author jakode2020
- * @since v1.0.0 7/13/2021
+ * @since v2.0.0 7/22/2021
  */
-sealed interface DataState<out M, out N> {
+sealed interface DataState<out M> {
 
-    data class Success<T, K>(val data: T, val status: K) : DataState<T, K>
+    data class Success<T>(val data: T) : DataState<T>
 
-    data class Error(val exception: Exception) : DataState<Nothing, Nothing>
+    data class Error(val exception: Exception) : DataState<Nothing>
 
-    object Loading : DataState<Nothing, Nothing>
+    object Loading : DataState<Nothing>
 }
